d627c0c0562ff1970f7036ab47b021f8b3136645
Sachin Kamath <kamathsachin@hotmail.com>
2021-03-15 20:41:52 -0500

    added the connector

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,55 +1,108 @@
-import React, { useState, useEffect } from "react";
+//import React, { useState, useEffect } from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
 import CardList from "../components/CardList";
 import SearchList from "../components/SearchList";
 import Scroll from "../components/Scroll";
 
-function App() {
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     robots: [],
-  //     searchfield: "",
-  //   };
-  // }
+import { setSearchField } from "../action";
 
-  // componentDidMount() {
-  //   fetch("https://jsonplaceholder.typicode.com/users")
-  //     .then((response) => response.json())
-  //     .then((users) => {
-  //       this.setState({ robots: users });
-  //     });
-  // }
+const mapStateToProps = (state) => {
+  return {
+    searchField: state.searchField,
+  };
+};
 
-  const [robots, setRobots] = useState([]);
-  const [searchfield, setSearchField] = useState("");
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+  };
+};
 
-  useEffect(() => {
+// function App() {
+//   // constructor() {
+//   //   super();
+//   //   this.state = {
+//   //     robots: [],
+//   //     searchfield: "",
+//   //   };
+//   // }
+
+//   // componentDidMount() {
+//   //   fetch("https://jsonplaceholder.typicode.com/users")
+//   //     .then((response) => response.json())
+//   //     .then((users) => {
+//   //       this.setState({ robots: users });
+//   //     });
+//   // }
+
+//   const [robots, setRobots] = useState([]);
+//   const [searchfield, setSearchField] = useState("");
+
+//   useEffect(() => {
+//     fetch("https://jsonplaceholder.typicode.com/users")
+//       .then((response) => response.json())
+//       .then((users) => {
+//         setRobots(users);
+//       });
+//   }, []);
+
+//   const onSearchChange = (event) => {
+//     setSearchField(event.target.value);
+//   };
+
+//   const filteredRobots = robots.filter((robot) => {
+//     return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+//   });
+
+//   console.log(filteredRobots);
+
+//   return (
+//     <div className="tc">
+//       <h1 className="f1">RoboFriends</h1>
+//       <SearchList searchChange={onSearchChange}></SearchList>
+//       <Scroll>
+//         <CardList robots={filteredRobots}></CardList>
+//       </Scroll>
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+    };
+  }
+
+  componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) => {
-        setRobots(users);
+        this.setState({ robots: users });
       });
-  }, []);
+  }
 
-  const onSearchChange = (event) => {
-    setSearchField(event.target.value);
-  };
+  render() {
+    const { robots } = this.state;
+    const { searchField, onSearchChange } = this.props;
+    const filteredRobots = robots.filter((robot) => {
+      return robot.name.toLowerCase().includes(searchField.toLowerCase());
+    });
 
-  const filteredRobots = robots.filter((robot) => {
-    return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-  });
+    console.log(filteredRobots);
 
-  console.log(filteredRobots);
-
-  return (
-    <div className="tc">
-      <h1 className="f1">RoboFriends</h1>
-      <SearchList searchChange={onSearchChange}></SearchList>
-      <Scroll>
-        <CardList robots={filteredRobots}></CardList>
-      </Scroll>
-    </div>
-  );
+    return (
+      <div className="tc">
+        <h1 className="f1">RoboFriends</h1>
+        <SearchList searchChange={onSearchChange}></SearchList>
+        <Scroll>
+          <CardList robots={filteredRobots}></CardList>
+        </Scroll>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
