098b2687a397903f910202c88131914466d72376
Sachin Kamath <kamathsachin@hotmail.com>
2021-03-15 20:16:19 -0500

    created the store and added the provider component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider, connect } from "react-redux";
+import { createStore } from "redux";
 import "./index.css";
 import App from "./containers/App";
 import reportWebVitals from "./reportWebVitals";
 import "tachyons";
+import { searchRobots } from "./reducer";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(searchRobots);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
