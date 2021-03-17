5f0da9e6bf12a2d086718e55f933593a99b682ac
Sachin Kamath <kamathsachin@hotmail.com>
2021-03-16 20:42:35 -0500

    final changes after implementing middleware and async operations

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,95 +5,38 @@ import CardList from "../components/CardList";
 import SearchList from "../components/SearchList";
 import Scroll from "../components/Scroll";
 
-import { setSearchField } from "../action";
+import { requestRobots, setSearchField } from "../action";
 
 const mapStateToProps = (state) => {
   return {
-    searchField: state.searchField,
+    searchField: state.searchRobots.searchField,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending,
+    error: state.requestRobots.error,
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
     onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => dispatch(requestRobots()),
   };
 };
 
-// function App() {
-//   // constructor() {
-//   //   super();
-//   //   this.state = {
-//   //     robots: [],
-//   //     searchfield: "",
-//   //   };
-//   // }
-
-//   // componentDidMount() {
-//   //   fetch("https://jsonplaceholder.typicode.com/users")
-//   //     .then((response) => response.json())
-//   //     .then((users) => {
-//   //       this.setState({ robots: users });
-//   //     });
-//   // }
-
-//   const [robots, setRobots] = useState([]);
-//   const [searchfield, setSearchField] = useState("");
-
-//   useEffect(() => {
-//     fetch("https://jsonplaceholder.typicode.com/users")
-//       .then((response) => response.json())
-//       .then((users) => {
-//         setRobots(users);
-//       });
-//   }, []);
-
-//   const onSearchChange = (event) => {
-//     setSearchField(event.target.value);
-//   };
-
-//   const filteredRobots = robots.filter((robot) => {
-//     return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-//   });
-
-//   console.log(filteredRobots);
-
-//   return (
-//     <div className="tc">
-//       <h1 className="f1">RoboFriends</h1>
-//       <SearchList searchChange={onSearchChange}></SearchList>
-//       <Scroll>
-//         <CardList robots={filteredRobots}></CardList>
-//       </Scroll>
-//     </div>
-//   );
-// }
-
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      robots: [],
-    };
-  }
-
   componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((users) => {
-        this.setState({ robots: users });
-      });
+    this.props.onRequestRobots();
   }
 
   render() {
-    const { robots } = this.state;
-    const { searchField, onSearchChange } = this.props;
+    const { searchField, onSearchChange, robots, isPending } = this.props;
     const filteredRobots = robots.filter((robot) => {
       return robot.name.toLowerCase().includes(searchField.toLowerCase());
     });
 
-    console.log(filteredRobots);
-
-    return (
+    return isPending ? (
+      <h1>Loading</h1>
+    ) : (
       <div className="tc">
         <h1 className="f1">RoboFriends</h1>
         <SearchList searchChange={onSearchChange}></SearchList>
--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider, connect } from "react-redux";
-import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import { createLogger } from "redux-logger";
+import thunkMiddleware from "redux-thunk";
 import "./index.css";
 import App from "./containers/App";
 import reportWebVitals from "./reportWebVitals";
 import "tachyons";
-import { searchRobots } from "./reducer";
+import { searchRobots, requestRobots } from "./reducer";
 
-const store = createStore(searchRobots);
+const logger = createLogger();
+const rootReducer = combineReducers({ searchRobots, requestRobots });
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunkMiddleware, logger)
+);
 
 ReactDOM.render(
   <Provider store={store}>
